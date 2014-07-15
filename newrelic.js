7ff4f7087e30d134798a1670dda347e501127bb0
Arthur Eyckerman <tuurleyck@gmail.com>
2014-07-15 21:31:34 +0000

    add newrelic.js

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['node-mongo-jade'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'dbbc060131e8835c7ebe9467e8d6c7cd7039a952',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
